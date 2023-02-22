aca343217bc2161dfbdc69667faf7d825943614d
ClaudioAresu <93395300+ClaudioAresu@users.noreply.github.com>
2023-02-22 18:39:31 +0100

    Create plot_loss_curve.py
    Returns separate loss curves for trainings and validation metrics

---
+++ plot_loss_curve.py
@@ -0,0 +1,27 @@
+#Let's create a function to visualize our loss and accuracy curves...
+
+def plot_loss_curve(history):
+  """
+  Returns separate loss curves for trainings and validation metrics
+  """
+  loss = history.history["loss"]
+  val_loss = history.history["val_loss"]
+
+  accuracy = history.history["accuracy"]
+  val_accuracy = history.history["val_accuracy"]
+
+  epochs = range(len(history.history["loss"]))
+
+  plt.figure()
+  plt.plot(epochs, loss, label="training_loss")
+  plt.plot(epochs, val_loss, label="val_loss")
+  plt.title("loss")
+  plt.xlabel("epochs")
+  plt.legend()
+
+  plt.figure()
+  plt.plot(epochs, accuracy, label="training_accuracy")
+  plt.plot(epochs, val_accuracy, label="val_accuracy")
+  plt.title("accuracy")
+  plt.xlabel("epochs")
+  plt.legend()
